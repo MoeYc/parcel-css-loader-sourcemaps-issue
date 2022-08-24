fe86ff3004b7e1b145263607cbf70aac34e2db89
Valentin Gurkov <vgurkov@liveareacx.com>
2022-08-24 12:23:33 +0300

    initial commit with minimal reproducible repo

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const parcelCSS = require('@parcel/css');
+
+
+module.exports = {
+    mode: 'production',
+    name: 'app_birkenstock/less',
+    entry: {
+        'css/style': './src/less/style.less',
+    },
+    plugins: [
+        // new RemoveEmptyScriptsPlugin({
+        //     enabled: true
+        // }),
+        new MiniCssExtractPlugin()
+    ],
+    devtool: 'cheap-module-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.less$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: false,
+                            sourceMap: true
+                        }
+                    },
+                    // {
+                    //     loader: 'postcss-loader',
+                    //     options: {
+                    //         postcssOptions: {
+                    //             sourceMap: true
+                    //         }
+                    //     }
+                    // },
+                    {
+                        loader: 'parcel-css-loader',
+                        options: {
+                            implementation: parcelCSS,
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        loader: 'less-loader',
+                        options: {
+                            lessOptions: {
+                                relativeUrls: false,
+                                sourceMap: true
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+}
